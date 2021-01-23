178b3fa49e145f2f2010253e11f9a67e41766a2e
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-01-23 17:51:11 -0300

    add new configuration in server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,21 @@
-const app = require('./app')
+require('./database')
+const routes = require('./routes')
 
-app.listen(5000)
+const express = require('express')
+const app = express();
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+})
+
+
+server.listen(5000)
